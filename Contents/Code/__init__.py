27764fd3a22293e534236c8f5650480c42ad9edd
Pierre <p.dellanave@gmail.com>
2011-03-25 00:53:07 -0700

    Converted to v2

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,35 +1,43 @@
-import sys
-from PMS import Plugin, Log, DB, Thread, XML, HTTP, JSON, RSS, Utils
-from PMS.MediaXML import MediaContainer, DirectoryItem, PhotoItem
-
 BP_PLUGIN_PREFIX   = "/photos/TheBigPicture"
+PLUGIN_TITLE       = "The Big Picture"
 BP_RSS_FEED        = "http://www.boston.com/bigpicture/index.xml"
+
+ART         = "art-default.jpg"
+ICON        = "icon-default.png"
 
 ####################################################################################################
 def Start():
-  Plugin.AddRequestHandler(BP_PLUGIN_PREFIX, HandlePhotosRequest, "The Big Picture", "icon-default.png", "art-default.jpg")
-  Plugin.AddViewGroup("ImageStream", viewMode="Pictures", contentType="items")
+  Plugin.AddPrefixHandler(BP_PLUGIN_PREFIX, MainMenu, PLUGIN_TITLE, ICON, ART)
+  Plugin.AddViewGroup("ImageStream", viewMode="Pictures", mediaType="items")
+  Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
+
+  MediaContainer.art       = R(ART)
+  MediaContainer.title1    = PLUGIN_TITLE
+  MediaContainer.viewGroup = "InfoList"
+  DirectoryItem.thumb      = R(ICON)
 
 ####################################################################################################
-def HandlePhotosRequest(pathNouns, count):
-  dir = MediaContainer("art-default.jpg", None, "The Big Picture")
-  
-  if count == 0:
-    feed = RSS.Parse(BP_RSS_FEED)
-    for item in feed.entries:
-        dir.AppendItem(DirectoryItem(Utils.EncodeStringToUrlPath(item.link)+'$'+item.title, item.title, ""))
-  elif count == 1:
-    (url,title) = pathNouns[0].split('$')
-    dir = MediaContainer("art-default.jpg", "ImageStream", "The Big Picture", title.replace('+',' '))
-    url = Utils.DecodeUrlPathToString(url)
-    i = 1
-    for photo in XML.ElementFromURL(url, True).xpath("//div[@class='bpBoth']"):
-      try:
-        src = photo.find('img').get('src')
-        summary = photo.xpath("div[@class='bpCaption']/text()")[0]
-        dir.AppendItem(PhotoItem(src, "Photo %d" % i, summary, None))
-        i += 1
-      except:
-        sys.stderr.write("No image found")
+def MainMenu():
+  dir = MediaContainer(viewGroup = "List")
+  feed = XML.ElementFromURL(BP_RSS_FEED)
+  for item in feed.xpath("//rss//channel//item"):
+    description = item.xpath(".//description")[0].text.replace('&gt;','>').replace('&lt','<')
+    thumb = HTML.ElementFromString(description).xpath(".//div[@class='bpImageTop']//img")[0].get('src')
+    url = item.xpath(".//link")[0].text
+    title = item.xpath(".//title")[0].text
+    dir.Append(Function(DirectoryItem(HandlePhotos,title,thumb=thumb),url = url, title = title))
 
-  return dir.ToXML()
+  return dir
+  
+def HandlePhotos(sender, url = "", title = ''):
+  dir = MediaContainer(viewGroup = "ImageStream")
+
+  for photo in HTML.ElementFromURL(url).xpath("//div[@class='bpBoth']"):
+    try:
+      src = photo.xpath('img')[0].get('src')
+      summary = photo.xpath("div[@class='bpCaption']/text()")[0]
+      dir.Append(PhotoItem(src, title, summary, None))
+    except:
+      pass
+    
+  return dir
